1d858f1c55eaa6a7d2ac55e40a2b5d2c50795f71
jezus <90801079+solnMarket@users.noreply.github.com>
2024-11-22 19:13:55 +0800

    Project is almost done
    Few issues came up in development
    User cannot add more books. this is stupid the app is a library I cant beieve I let it come to this.
    User cannot delete books.
    Not all books can change read status. only books that already exist, when the DOM loads.
    
    I need to plan out my application next time. I brute force them and this is the result. A library that can't store more that 1 book.
    I should blur out the background when the modal is active.
    
    I need to get better.

--- script.js
+++ script.js
@@ -13,30 +13,100 @@ function Book(title,author,pages,read,picture){
 
 
 document.addEventListener('DOMContentLoaded',function(){
+    const title = document.getElementById('book-title')
+    const author = document.getElementById('book-author')
+    const pages = document.getElementById('book-pages')
+    const status = document.getElementById('read-status')
+    const bookCover = document.getElementById('fileInput')
+
     const addBook = document.querySelector('.add');
     const close = document.querySelector('.close')
     const modal = document.querySelector('.modal')
     const submit = document.querySelector('.submit')
+    const form = document.getElementById('form')
+    
+    form.addEventListener('submit', function(event){
+        event.preventDefault();
+
+        
+
+        Book = new Book(title.value,author.value,pages.value,status.value,bookCover.value)
+        let card = document.createElement('div')
+        let bookImg=document.createElement('div')
+        let bookDesc = document.createElement('div')
+        let img=document.createElement('img')
+        let button=document.createElement('button')
+        let bookTitle=document.createElement('div')
+        let bookAuthor=document.createElement('div')
+        let readStatus=document.createElement('div')
+        let numPages=document.createElement('div')
+
+        card.classList.add('card')
+        mainContent=document.querySelector('.main-content')
+        mainContent.appendChild(card)
+
+        bookImg.classList.add('book-img')
+        card.appendChild(bookImg)
+        bookDesc.classList.add('book-desc')
+        card.appendChild(bookDesc)
+
+        
+        bookImg.appendChild(img)
+        bookImg.appendChild(button)
+
+        bookDesc.appendChild(bookTitle)
+        bookDesc.appendChild(bookAuthor)
+        bookDesc.appendChild(numPages)
+
+        img.style.src=Book.picture
+        bookTitle.textContent=Book.title
+        bookAuthor.textContent=Book.author
+        numPages.textContent=Book.pages
+        button.textContent=Book.read
+        modal.classList.remove('active')
+        
+        const file = bookCover.files[0]
+        const reader = new FileReader()
+        reader.onload = () => {
+            img.src=reader.result
+            img.style.display="block"
+        }
+        reader.readAsDataURL(file)
+
+        let but = document.querySelectorAll('.book-img button')
+
+        
+
+    })
+
+    let but = document.querySelectorAll('.book-img button')
+
+    but.forEach(n => {
+        n.addEventListener('click', function(){
+            if(n.textContent=="Read"){
+                n.textContent="Not Read"
+            }else if(n.textContent=="Not Read"){
+                n.textContent="Read"
+            }
+        })
+        
+    })
+
     addBook.addEventListener('click', function(){
         
+        title.value=""
+        author.value=""
+        pages.value=""
+    
+        bookCover.value=""
         modal.classList.add('active')
     })
     close.addEventListener('click', function(){
         modal.classList.remove('active')
     })
-    submit.addEventListener('click', function(){
-        const title = document.getElementById('book-title').textContent
-        const author = document.getElementById('book-author').textContent
-        const pages = document.getElementById('book-pages').textContent
-        const status = document.getElementById('book-status').textContent
-        const bookCover = document.getElementById('fileInput').textContent
-        
-        Book = new Book(title,author,pages,status,bookCover)
-        const card = document.createElement('div')
-        card.classList.add('card')
-        mainContent=document.querySelector('main-content')
-        mainContent.appendChild(card)
-    })
+
+
+
 
 })
 
